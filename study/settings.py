81ef9181c81134f52d88beff8563b5c4ead9f7ee
tolujohn <tolujohnofficial@gmail.com>
2023-01-23 11:35:41 +0100

    settins file edited

--- study/settings.py
+++ study/settings.py
@@ -134,11 +134,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# #STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+# MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
+
 
 
 
